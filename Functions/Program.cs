6b63390e9d464b64d036d9ba2689e89826e401b0
Tomasz <n1kula@wp.pl>
2024-02-02 22:24:39 +0100

    funct, action

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Functions
 {
@@ -42,6 +43,21 @@ namespace Functions
             {
                 Console.WriteLine(person.FirstName);
             }
+
+            Func<int, int> pot = x => x * x;
+            Console.WriteLine(pot(2));
+
+            Func<int, int, int> add = (x, y) => x + y;
+            Func<int, int, int> add2 = (x, y) =>
+            {
+                var result = x + y;
+                return result;
+            };
+            Console.WriteLine(add(2, 3));
+
+
+            //return void
+            Action<int> write = x => Console.WriteLine(x);
         }
 
         private static bool StartsWithM(Employee employee)
