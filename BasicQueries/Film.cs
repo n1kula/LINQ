a4a16319b38f6045c36e48f98d163474dc39c37a
Tomasz <n1kula@wp.pl>
2024-02-02 23:47:13 +0100

    custom filter

---
+++ BasicQueries/Film.cs
@@ -0,0 +1,11 @@
+﻿namespace BasicQueries
+{
+    internal class Film
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public double Rating { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ BasicQueries/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace BasicQueries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Film>
+            {
+                new Film {Title = "Bad Boys", Type = "Comedy", Rating = 8.2, Year = 2005},
+                new Film {Title = "Gladiator", Type = "Drama", Rating = 7.2, Year = 2010},
+                new Film {Title = "Kiler", Type = "Comedy", Rating = 5.2, Year = 1999},
+            };
+
+            var query = movies.MyFilter(f => f.Year > 2005);
+
+            foreach (var film in query)
+            {
+                Console.WriteLine(film.Title);
+            }
+        }
+    }
+}
