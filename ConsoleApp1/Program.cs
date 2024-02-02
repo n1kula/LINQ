30dcda3d95409335b7819dbfbba96e2690010ea7
Tomasz <n1kula@wp.pl>
2024-02-02 12:38:14 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\windows";
+            ShowFilesWithoutLinq(path);
+        }
+
+        private static void ShowFilesWithoutLinq(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] files = dir.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine($"{file.Name} : {file.Length}");
+            }
+        }
+
+        public class FileInfoComparer : IComparer<FileInfo>
+        {
+            public int Compare(FileInfo x, FileInfo y)
+            {
+                return x.Length.CompareTo(y.Length);
+            }
+        }
+    }
+}
